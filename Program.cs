f497962acb6f31e0859a1ce51bda85593fa2b78f
abdurozikov.kuvonchbek@gmail.com <abdurozikov.kuvonchbek@gmail.com>
2023-03-08 19:02:56 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace input_output
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kvadratning tomoni a berilgan.
+            //P = 4 * a formula orqali perimetri aniqlansin.
+            Console.Write("Kvadratning tomonini kiriting a = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            int p = 4 * a;
+            Console.Write("Kvadratning peremetri " + p + " ga teng");
+
+
+        }
+    }
+}
